404e0b0a4bee63f2858d84f902253a36904fde08
Tejaswini Mali <99062996+Malitejaswini@users.noreply.github.com>
2022-09-11 02:41:31 -0400

    Create sqrt.py

---
+++ sqrt.py
@@ -0,0 +1,6 @@
+#Find Squareroot of given number using pow function.
+import math
+num=float(input("Enter the Number"))
+sqRoot=math.pow(num,0.5)
+print("The Given Number =",num)
+print("The Squareroot is =",sqRoot)
